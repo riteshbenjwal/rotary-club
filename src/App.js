3c51603b23d1b93ed2fcf610509ca1ea2fda1a2f
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-08-21 12:04:52 +0530

    test-fix

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from "react";
 import Home from "./Home";
-import { Route, Switch } from "react-router";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Error from "./pages/Error";
-import Message from "./pages/Message";
+// import { Route, Switch } from "react-router";
+// import About from "./pages/About";
+// import Contact from "./pages/Contact";
+// import Error from "./pages/Error";
+// import Message from "./pages/Message";
 
 const App = () => {
   return (
     <>
-      <Switch>
+      {/* <Switch>
         <Route exact path="/" component={Home}></Route>
-        <Route path="/about" component={About}></Route>
-        <Route path="/contact" component={Contact}></Route>
-        <Route path="/error" component={Error}></Route>
-        <Route path="/message" component={Message}></Route>
-      </Switch>
+        <Route exact path="/about" component={About}></Route>
+        <Route exact path="/contact" component={Contact}></Route>
+        <Route exact path="/error" component={Error}></Route>
+        <Route exact path="/message" component={Message}></Route>
+      </Switch> */}
+      <Home/>
     </>
   );
 };

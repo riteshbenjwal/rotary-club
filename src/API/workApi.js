ebb9b214f7106ba76fdb8cafa1715c333a4693e3
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-08-21 12:17:54 +0530

    our causes text

--- src/API/workApi.js
+++ src/API/workApi.js
@@ -3,37 +3,37 @@ const workapi = [
     id: 1,
     logo: "fas fa-3x fa-hand-peace",
     title: "Peace & Conflict Prevention",
-    info: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
+    info: "Rotary fosters understanding across cultures. We train adults and young leaders to prevent and mediate conflict and help refugees who have fled dangerous areas.",
   },
   {
     id: 2,
     logo: "fas fa-first-aid",
     title: "Maternal & Child Health",
-    info: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
+    info: "We expand access to quality medical care, sanitation, education and economic opportunities so mothers and their children can live and grow stronger.",
   },
   {
     id: 3,
     logo: "fas fa-school",
     title: "Education & Community Development",
-    info: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
+    info: "We carry out service projects that enhance economic and community development and create opportunities for decent and productive work for young and old.",
   },
   {
     id: 4,
     logo: "fas fa-stethoscope",
     title: "Disease Prevention & Treatment",
-    info: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
+    info: "We are resolved to rid the world of polio forever. We also educate and equip communities to stop other diseases and expand access to health care.",
   },
   {
     id: 5,
     logo: "fas fa-hand-holding-water",
     title: "Water and Sanitation",
-    info: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
+    info: "We help bring sustainable clean water, sanitation, and hygiene to more people every day. We don’t just build wells and walk away.",
   },
   {
     id: 6,
     logo: "fas fa-user-graduate",
     title: "Basic Education & Literacy",
-    info: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
+    info: "Our goal is to strengthen the capacity of communities to support basic education and literacy, reduce gender disparity in education, and increase adult literacy.",
   },
 ];
 

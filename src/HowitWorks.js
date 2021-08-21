0bd7e9871acce469d7ac46f9e60a3e9cd67285dc
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-08-21 11:09:44 +0530

    error

--- src/HowitWorks.js
+++ src/HowitWorks.js
@@ -13,7 +13,7 @@ const HowitWorks = () => {
               const { id, logo, title, info } = curElem;
               return (
                 <>
-                  <div className="col-12 col-lg-4 text-center work-container-subdiv">
+                  <div key={id} className="col-12 col-lg-4 text-center work-container-subdiv">
                     <i class={`fontawesome-style ${logo}`}></i>
                     <h2 className="sub-heading">{title}</h2>
                     <p className="main-hero-para w-100">{info}</p>
--- src/navbar.js
+++ src/navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
       <section classNameName="navbar-bg">
         <nav className="navbar navbar-expand-lg navbar-light  ">
           <div className="container">
-            <a className="navbar-brand" href="#">
+            <a className="navbar-brand" href="/">
               <img
                 className="img-fluid logo"
                 src="./images/logo.png"
@@ -32,17 +32,13 @@ const Navbar = () => {
             <div className={`collapse navbar-collapse ${show ? "show" : ""}`}>
               <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <NavLink
-                    className="nav-link active"
-                    aria-current="page"
-                    to="/"
-                  >
+                  <NavLink className="nav-link active" aria-current="page" to="/">
                     Home
                   </NavLink>
                 </li>
                 <li className="nav-item">
                   <NavLink className="nav-link" to="/about">
-                    About us
+                   About us
                   </NavLink>
                 </li>
                 <li className="nav-item">
@@ -52,11 +48,8 @@ const Navbar = () => {
                 </li>
               </ul>
               <form className="d-flex">
-                <button
-                  className="btn btn-style btn-style-border"
-                  type="submit"
-                >
-                  <NavLink className="nav-link m" to="/contact">
+                <button className="btn btn-style btn-style-border" type="submit">
+                <NavLink className="nav-link" to="/contact">
                     Contact
                   </NavLink>
                 </button>
